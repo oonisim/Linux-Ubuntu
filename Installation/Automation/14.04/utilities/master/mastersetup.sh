a20ac8562aedcb1c2cd8a2f59212026804d717e5
oonisim <monishi@wynyardgroup.com>
2016-11-21 11:36:56 +1100

    Updated to configure Puppet repository and not use ${which puppet}

--- Installation/Automation/14.04/utilities/master/mastersetup.sh
+++ Installation/Automation/14.04/utilities/master/mastersetup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -u
 
+#--------------------------------------------------------------------------------
+# Make sure that name resolution is available to resolve github, etc.
+#--------------------------------------------------------------------------------
+
 os="ubuntu"
 OS="Ubuntu"
 OSVERSION="14.04"
@@ -15,6 +19,8 @@ cgigrp="wynadmin"
 wwwdir="/var/www"
 cgidir="/var/www/cgi-bin/installations/${os}/${OSVERSION}"
 
+puppet="/opt/puppetlabs/puppet/bin/puppet"
+
 echo '--------------------------------------------------------------------------------'
 echo 'Instaling Git'
 echo '--------------------------------------------------------------------------------'
@@ -24,20 +30,29 @@ sudo apt-get install -y tree
 echo '--------------------------------------------------------------------------------'
 echo 'Creating Git repository'
 echo '--------------------------------------------------------------------------------'
-rm -r -f ${repodir}/${repository}
+rm -r -f ${repodir}
 mkdir ${repodir}
 cd ${repodir} && git init && git clone ${giturl}
+
+
+echo '--------------------------------------------------------------------------------'
+echo ' Setting up Puppet repository ...'
+echo ' Disable this when using no internet environment'
+echo '--------------------------------------------------------------------------------'
+wget https://apt.puppetlabs.com/puppetlabs-release-pc1-trusty.deb
+sudo dpkg -i puppetlabs-release-pc1-trusty.deb
 
 echo '--------------------------------------------------------------------------------'
 echo 'Installing Puppet Server ...'
 echo 'See https://tickets.puppetlabs.com/browse/SERVER-528'
 echo '--------------------------------------------------------------------------------'
+sudo apt-get update
 sudo apt-get install -y puppetserver
-sudo $(which puppet) resource service puppet ensure=stopped
-sudo $(which puppet) resource service puppetserver ensure=stopped
+sudo ${puppet} resource service puppet ensure=stopped
+sudo ${puppet} resource service puppetserver ensure=stopped
 sudo rm -r -f /etc/puppetlabs/puppet/ssl
-sudo $(which puppet) cert list -a
-sudo $(which puppet) master --no-daemonize --verbose
+sudo ${puppet} cert list -a
+sudo ${puppet} master --no-daemonize --verbose
 
 echo '--------------------------------------------------------------------------------'
 echo 'Copying to /etc puppet module and manifets before starting puppet server'
@@ -47,7 +62,7 @@ eval "sudo cp -r ${gitdir}/etc/* /etc/"
 echo '--------------------------------------------------------------------------------'
 echo 'Starting Puppet Server'
 echo '--------------------------------------------------------------------------------'
-sudo $(which puppet) resource service puppetserver ensure=running
+sudo ${puppet} resource service puppetserver ensure=running
 sudo service puppetserver start
 
 
@@ -58,8 +73,8 @@ echo 'Run puppet agent to configure the master server itself ...'
 echo '--------------------------------------------------------------------------------'
 echo "143.96.102.150 puppet.wynyarddemo.local" | sudo tee -a /etc/hosts
 
-sudo $(which puppet) cert sign --all
-sudo $(which puppet) agent --test
+sudo ${puppet} cert sign --all
+sudo ${puppet} agent --test
 
 echo '--------------------------------------------------------------------------------'
 echo 'Copying Ubuntu auto installation start into HTTPD directories.'
